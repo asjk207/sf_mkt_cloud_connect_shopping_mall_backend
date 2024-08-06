8d4f4ebe282088d20392b0f18ea88b65ec060dab
asjk207 <jhkyoon@naver.com>
2024-08-06 17:35:53 +0900

    첫 번째 커밋

---
+++ sf_mkt_cloud_connect_shopping_mall_backend/src/main/java/com/shoppingmall/mktcloud/springboot/vscode/sf_mkt_cloud_connect_shopping_mall_backend/MainController.java
@@ -0,0 +1,13 @@
+package com.shoppingmall.mktcloud.springboot.vscode.sf_mkt_cloud_connect_shopping_mall_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainController {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainController.class, args);
+	}
+
+}
---
+++ sf_mkt_cloud_connect_shopping_mall_backend/src/test/java/com/shoppingmall/mktcloud/springboot/vscode/sf_mkt_cloud_connect_shopping_mall_backend/MainControllerTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmall.mktcloud.springboot.vscode.sf_mkt_cloud_connect_shopping_mall_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainControllerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
